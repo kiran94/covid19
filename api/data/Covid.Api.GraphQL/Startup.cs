3f74ef5811888fbbdff98f3e0fed21780f70bc6c
Kiran94 <kiran_patel94@hotmail.com>
2020-06-09 21:45:28 +0100

    Cleaned up Api Code

--- api/data/Covid.Api.GraphQL/Startup.cs
+++ api/data/Covid.Api.GraphQL/Startup.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using Covid.Api.Common.DataAccess;
-
 namespace Covid.Api.GraphQL
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Covid.Api.Common.DataAccess;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)

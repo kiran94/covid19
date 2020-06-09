c27bc8d13948bf1004d332d8705e1dc71a739122
Kiran94 <kiran_patel94@hotmail.com>
2020-06-09 05:47:21 +0100

    Removed comments in logger

--- etl/covid/core/logger.py
+++ etl/covid/core/logger.py
@@ -11,15 +11,11 @@ class CovidLogger(logging.Logger):
                  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
         super().__init__(name, level)
 
-        # console_handler = logging.StreamHandler()
-        # console_handler.setLevel(level)
-
         formatter = logging.Formatter(format)
-        # console_handler.setFormatter(formatter)
-        # self.addHandler(console_handler)
 
         handler = RainbowLoggingHandler(sys.stderr, color_funcName=('black', 'yellow', True))
         handler.setFormatter(formatter)
         self.addHandler(handler)
+
 
 logging.getLogger('sqlalchemy.engine.base.Engine').setLevel(logging.WARNING)

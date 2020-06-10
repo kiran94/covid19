d2c95d3655a1f6b32d133848d167330e5d472737
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2020-06-10 23:25:54 +0100

    Added OpenTracing to the GraphQL API (#3)

--- api/data/Covid.Api.GraphQL/Extensions/GraphQLExtensions.cs
+++ api/data/Covid.Api.GraphQL/Extensions/GraphQLExtensions.cs
@@ -1,6 +1,9 @@
 namespace Covid.Api.GraphQL.Extensions
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using global::GraphQL.Types;
+    using OpenTracing;
 
     public static class GraphQLExtensions
     {
@@ -17,6 +20,45 @@ namespace Covid.Api.GraphQL.Extensions
 
             value = default;
             return false;
+        }
+
+        public static ISpan WithGraphQLTags(this ISpan span, ResolveFieldContext<object> context)
+        {
+            foreach (var argument in context.Arguments)
+            {
+                if (argument.Value is List<object> list)
+                {
+                    span.SetTag($"collection.{argument.Key}", true);
+
+                    if (list.Count == 1)
+                    {
+                        span.SetTag(argument.Key, list[0].ToString());
+                    }
+                    else
+                    {
+                        span.Log(list.Select(x => KeyValuePair.Create(argument.Key, x)));
+                    }
+                }
+                else if (argument.Value is object[] array)
+                {
+                    span.SetTag($"collection.{argument.Key}", true);
+
+                    if (array.Count() == 1)
+                    {
+                        span.SetTag(argument.Key, array[0].ToString());
+                    }
+                    else
+                    {
+                        span.Log(array.Select(x => KeyValuePair.Create(argument.Key, x)));
+                    }
+                }
+                else
+                {
+                    span.SetTag(argument.Key, argument.Value.ToString());
+                }
+            }
+
+            return span;
         }
     }
 }

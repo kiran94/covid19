6bdd2679fc3947e86dee63e41a4c20ccb74d8041
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2020-06-12 09:56:48 +0100

    Add ElasticSearch and Structured Logging in API (#4)
    * Added Serilog Logging
    
    * Moved docker compose file
    
    * Added docker volume folder to gitignore
    
    * Updated volumes in docker compose
    
    * Added ElasticSearch and Updated API to log to it
    
    * Updated elasticsearch and kibana versions
    
    * restricted etl requirements txt tornado

--- api/data/Covid.Api.GraphQL/Program.cs
+++ api/data/Covid.Api.GraphQL/Program.cs
@@ -1,26 +1,75 @@
 namespace Covid.Api.GraphQL
 {
+    using System;
+    using System.Reflection;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
+    using Serilog;
+    using Serilog.Sinks.Elasticsearch;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ConfigureLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+                Log.CloseAndFlush();
+                throw;
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static void ConfigureLogger()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureKestrel(options => options.AllowSynchronousIO = true);
-                }).ConfigureLogging((context, logging) =>
+                    webBuilder.ConfigureKestrel(kestrel => kestrel.AllowSynchronousIO = true);
+                })
+                .ConfigureAppConfiguration(configuration =>
                 {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                });
+                    configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    configuration.AddJsonFile(
+                        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                        optional: true);
+                })
+                .UseSerilog();
     }
 }

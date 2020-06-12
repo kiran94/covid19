c65d09f29f6324cc39a721d96ffb76be7fd6aabc
Kiran94 <kiran_patel94@hotmail.com>
2020-06-12 20:56:15 +0100

    Centralised GraphQL Parameters

--- api/Covid.Api.GraphQL/Query/AppQuery.cs
+++ api/Covid.Api.GraphQL/Query/AppQuery.cs
@@ -1,7 +1,8 @@
-using System.Threading;
-using System;
+
 namespace Covid.Api.GraphQL.Query
 {
+    using System.Threading;
+    using System;
     using System.Linq;
     using Covid.Api.Common.DataAccess;
     using Covid.Api.Common.Entities;
@@ -24,9 +25,9 @@ namespace Covid.Api.GraphQL.Query
                 description: "Get Country Information",
                 arguments: new QueryArguments()
                 {
-                    new QueryArgument<StringGraphType>() { Name = "query", Description = "Fuzzy Search Country/Region/Province/State/County" },
-                    new QueryArgument<IntGraphType>() { Name = "take", Description = "(Pagination) only take specified number of results" },
-                    new QueryArgument<IntGraphType>() { Name = "skip", Description = "(Pagination) skip the specified number of results" }
+                    Parameters.Argument<StringGraphType>(Parameters.Queries),
+                    Parameters.Argument<IntGraphType>(Parameters.Take),
+                    Parameters.Argument<IntGraphType>(Parameters.Skip)
                 },
                 resolve: async context =>
                 {
@@ -39,7 +40,7 @@ namespace Covid.Api.GraphQL.Query
                         .ThenBy(x => x.ProvinceState)
                         .AsQueryable();
 
-                    if (context.TryGetArgument<string>("query", out var query))
+                    if (context.TryGetArgument<string>(Parameters.Queries, out var query))
                     {
                         query = "%" + query + "%";
                         countries = countries.Where(
@@ -48,8 +49,8 @@ namespace Covid.Api.GraphQL.Query
                                 || EF.Functions.ILike(x.County, query));
                     }
 
-                    if (context.TryGetArgument<int>("take", out var take)) countries = countries.Take(take);
-                    if (context.TryGetArgument<int>("skip", out var skip)) countries = countries.Skip(skip);
+                    if (context.TryGetArgument<int>(Parameters.Take, out var take)) countries = countries.Take(take);
+                    if (context.TryGetArgument<int>(Parameters.Skip, out var skip)) countries = countries.Skip(skip);
 
                     return await countries
                         .OrderBy(x => x.CountryRegion)
@@ -62,13 +63,13 @@ namespace Covid.Api.GraphQL.Query
                 name: "timeseries",
                 description: "Gets the COVID-19 Timeseries Data",
                 arguments: new QueryArguments() {
-                    new QueryArgument<ListGraphType<StringGraphType>>() { Name = "country_region", Description = "The Country/Region(s) to Filter on"},
-                    new QueryArgument<ListGraphType<StringGraphType>>() { Name = "province_state", Description = "The Province/State(s) to Filter on"},
-                    new QueryArgument<ListGraphType<StringGraphType>>() { Name = "counties", Description = "The Counties to Filter on" },
-                    new QueryArgument<ListGraphType<StringGraphType>>() { Name = "fields", Description = "The Field to Filter on" },
-                    new QueryArgument<ListGraphType<DateTimeGraphType>>() { Name = "dates", Description = "The Time to Filter on" },
-                    new QueryArgument<IntGraphType>() { Name = "take", Description = "(Pagination) only take specified number of results" },
-                    new QueryArgument<IntGraphType>() { Name = "skip", Description = "(Pagination) skip the specified number of results" }
+                    Parameters.Argument<ListGraphType<StringGraphType>>(Parameters.CountryRegion),
+                    Parameters.Argument<ListGraphType<StringGraphType>>(Parameters.ProvinceState),
+                    Parameters.Argument<ListGraphType<StringGraphType>>(Parameters.Counties),
+                    Parameters.Argument<ListGraphType<StringGraphType>>(Parameters.Fields),
+                    Parameters.Argument<ListGraphType<DateTimeGraphType>>(Parameters.Dates),
+                    Parameters.Argument<IntGraphType>(Parameters.Take),
+                    Parameters.Argument<IntGraphType>(Parameters.Skip)
                 },
                 resolve: async context =>
                 {
@@ -79,33 +80,33 @@ namespace Covid.Api.GraphQL.Query
 
                     var timeseries = sql.Set<TimeSeries>().OrderByDescending(x => x.Date).AsQueryable();
 
-                    if (context.TryGetArgument<List<string>>("country_region", out var countries))
+                    if (context.TryGetArgument<List<string>>(Parameters.CountryRegion, out var countries))
                     {
                         timeseries = timeseries.Where(x => countries.Contains(x.CountryRegion));
                     }
 
-                    if (context.TryGetArgument<List<string>>("province_state", out var provinces))
+                    if (context.TryGetArgument<List<string>>(Parameters.ProvinceState, out var provinces))
                     {
                         timeseries = timeseries.Where(x => provinces.Contains(x.ProvinceState));
                     }
 
-                    if (context.TryGetArgument<List<string>>("counties", out var counties))
+                    if (context.TryGetArgument<List<string>>(Parameters.Counties, out var counties))
                     {
                         timeseries = timeseries.Where(x => counties.Contains(x.County));
                     }
 
-                    if (context.TryGetArgument<List<string>>("fields", out var fields))
+                    if (context.TryGetArgument<List<string>>(Parameters.Fields, out var fields))
                     {
                         timeseries = timeseries.Where(x => fields.Contains(x.Field));
                     }
 
-                    if (context.TryGetArgument<List<DateTime>>("dates", out var dates))
+                    if (context.TryGetArgument<List<DateTime>>(Parameters.Dates, out var dates))
                     {
                         timeseries = timeseries.Where(x => dates.Contains(x.Date));
                     }
 
-                    if (context.TryGetArgument<int>("skip", out var skip)) timeseries = timeseries.Skip(skip);
-                    if (context.TryGetArgument<int>("take", out var take)) timeseries = timeseries.Take(take);
+                    if (context.TryGetArgument<int>(Parameters.Skip, out var skip)) timeseries = timeseries.Skip(skip);
+                    if (context.TryGetArgument<int>(Parameters.Take, out var take)) timeseries = timeseries.Take(take);
 
                     return await timeseries.ToListAsync(context.CancellationToken);
                 });

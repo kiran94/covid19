beb2687488274fa89596b522947455bf5c3d958d
Kiran94 <kiran_patel94@hotmail.com>
2020-07-04 21:58:06 +0100

    Added icons to navigation

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -11,16 +11,18 @@
       <template v-slot:extension>
         <v-tabs align-with-title class="mt-2 secondary">
           <v-tab class="white--text" to="/" nuxt>Home</v-tab>
-          <v-tab class="white--text" to="/GlobalOverview" nuxt
-            >Global Overview</v-tab
+          <v-tab class="white--text" to="/GlobalOverview" nuxt>
+            <v-icon class="mr-1">mdi-earth</v-icon> Global Overview</v-tab
           >
           <v-tab class="white--text" to="/CountryOverview" nuxt
-            >Country Overview</v-tab
+            ><v-icon class="mr-1">mdi-island</v-icon>Country Overview</v-tab
           >
           <v-tab class="white--text" to="/CountryComparison" nuxt
-            >Country Comparison</v-tab
+            ><v-icon class="mr-1">mdi-compare</v-icon>Country Comparison</v-tab
           >
-          <v-tab class="white--text" to="/Fields" nuxt>Fields</v-tab>
+          <v-tab class="white--text" to="/Fields" nuxt
+            ><v-icon class="mr-1">mdi-table-arrow-left</v-icon>Fields</v-tab
+          >
         </v-tabs>
       </template>
     </v-app-bar>

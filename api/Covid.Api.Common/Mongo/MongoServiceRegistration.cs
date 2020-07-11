46348f838c748b3a06013d4fa50d7c2fba59566e
Kiran94 <kiran_patel94@hotmail.com>
2020-07-11 22:27:17 +0100

    Added extensions for mongo interceptors and registrations

---
+++ api/Covid.Api.Common/Mongo/MongoServiceRegistration.cs
@@ -0,0 +1,40 @@
+namespace Covid.Api.Common.Mongo
+{
+    using System;
+    using Covid.Api.Common.DataAccess;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using MongoDB.Driver;
+    using MongoDB.Driver.Core.Events;
+
+    public static class MongoServiceRegistration
+    {
+        public static IServiceCollection AddMongoService<TService, TImplementation>(
+            this IServiceCollection services,
+            string connectionString,
+            string database)
+            where TService: class
+            where TImplementation : TService
+            {
+                Console.WriteLine(connectionString);
+                Console.WriteLine(database);
+
+                services.AddSingleton<TService>(x => {
+
+                    var settings = MongoClientSettings.FromConnectionString(connectionString);
+                    settings.ClusterConfigurator = cb =>
+                    {
+                        cb.Subscribe<CommandStartedEvent>(
+                            MongoInterceptors.CommandStartedEvent(x.GetRequiredService<ILogger<IMongoDatabase>>()));
+                    };
+
+                    var mongoDatabase = new MongoClient(settings).GetDatabase(database);
+
+                    var impl = (TImplementation)Activator.CreateInstance(typeof(TImplementation), new MongoRepository(mongoDatabase));
+                    return impl;
+                });
+
+                return services;
+            }
+    }
+}

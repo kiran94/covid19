88d0f9e19dcc9b1b8df7af6b0c76188c6fae2f29
Kiran94 <kiran_patel94@hotmail.com>
2020-07-17 15:48:55 +0100

    Implemented Covid Data Service

--- api/Covid.Api.Grpc/Services/CovidDataService.cs
+++ api/Covid.Api.Grpc/Services/CovidDataService.cs
@@ -6,20 +6,73 @@ namespace Covid.Api.Grpc.Services
     using System.Threading.Tasks;
     using global::Grpc.Core;
     using Microsoft.Extensions.Logging;
+    using Covid.Api.Common.Services.TimeSeries;
+    using Microsoft.EntityFrameworkCore;
+    using Google.Protobuf.WellKnownTypes;
 
     public class CovidDataService : CovidService.CovidServiceBase
     {
         private readonly ILogger<CovidDataService> logger;
+        private readonly ITimeSeriesService timeseries;
 
-        public CovidDataService(ILogger<CovidDataService> logger)
+        public CovidDataService(ILogger<CovidDataService> logger, ITimeSeriesService timeseries)
         {
             this.logger = logger;
+            this.timeseries = timeseries;
         }
 
-        public override Task<CovidResponseCollection> Get(CovidRequest request, ServerCallContext context)
+        public override async Task<CovidResponseCollection> Get(CovidRequest request, ServerCallContext context)
         {
-            this.logger.LogInformation("Got Request!");
-            throw new NotImplementedException();
+            // Construct Query
+            var data = this.timeseries.Query();
+
+            if (!string.IsNullOrWhiteSpace(request.CountryRegion))
+                data = data.Where(x => x.CountryRegion == request.CountryRegion);
+
+            request.ProvinceState = request.ProvinceState ?? "";
+            data = data.Where(x => x.ProvinceState == request.ProvinceState);
+
+            request.County = request.County ?? "";
+            data = data.Where(x => x.County == request.County);
+
+            if (request.Fields.Any())
+            {
+                data = data.Where(x => request.Fields.Contains(x.Field));
+            }
+
+            if (request.DatesCase == CovidRequest.DatesOneofCase.RelativeDates)
+            {
+                throw new NotImplementedException("Relative Dates not Implemented");
+            }
+            else
+            {
+                var dates = request.AbsoluteDates.Dates.Select(x => x.ToDateTime().Date);
+                data = data.Where(x => dates.Contains(x.Date));
+            }
+
+            // Get Data
+            var result = await data.ToListAsync(context.CancellationToken);
+
+            result.ForEach(x =>
+            {
+                x.Date = DateTime.SpecifyKind(x.Date.Date, DateTimeKind.Utc);
+            });
+
+            // Construct Response
+            var covidResponses = result.Select(x => new CovidResponse()
+            {
+                CountryRegion = x.CountryRegion,
+                ProvinceState = x.ProvinceState,
+                County = x.County,
+                Field = x.Field,
+                Date =  Timestamp.FromDateTime(x.Date),
+                Value = x.Value ?? 0
+            }).ToList();
+
+            var covidResponsesCollection = new CovidResponseCollection();
+            covidResponsesCollection.Response.AddRange(covidResponses);
+
+            return covidResponsesCollection;
         }
     }
 }
--- api/Covid.Api.Grpc/Startup.cs
+++ api/Covid.Api.Grpc/Startup.cs
@@ -54,12 +54,11 @@
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
+                endpoints.MapGrpcService<CovidDataService>();
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                // });
             });
         }
     }
